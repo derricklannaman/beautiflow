264070e1798d51c0683a2df2a32b23ed98407161
Derrick Lannaman <derricklannaman@gmail.com>
2012-12-05 18:55:40 -0500

    add Stylist.find(params[:sty..id] to client controllers

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,38 +1,43 @@
 class ClientsController < ApplicationController
   def index
-    @clients = Client.all
+    stylist = Stylist.find(params[:stylist_id])
+    @clients = stylist.clients
   end
 
   def new
-    @client = Client.new
+    @stylist = Stylist.find(params[:stylist_id])
+    @client = @stylist.clients.new
   end
 
   def create
+    @stylist = Stylist.find(params[:stylist_id])
     @client = Client.new(params[:client])
     if @client.save
-      redirect_to stylist_client_path(@client)
+      redirect_to stylist_client_path(@stylist, @client)
     else
       render :new
     end
   end
 
   def edit
+    @stylist = Stylist.find(params[:stylist_id])
     @client = Client.find(params[:id])
   end
 
   def update
+    @stylist = Stylist.find(params[:stylist_id])
     @client = Client.find(params[:id])
     @client.update_attributes(params[:client])
       redirect_to stylist_client_path(@client)
   end
 
   def show
+    @stylist = Stylist.find(params[:stylist_id])
     @client = Client.find(params[:id])
   end
 
   def destroy
     Client.find(params[:id]).destroy
-    # redirect_to clients_path
     redirect_to stylist_clients_path
   end
 end

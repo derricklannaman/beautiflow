d4b552308eac252c54ad3ede814075d71583090c
Derrick Lannaman <derricklannaman@gmail.com>
2013-01-06 22:34:46 -0500

    uncomment(activate) validations for stylist sign ups

--- app/models/stylist.rb
+++ app/models/stylist.rb
@@ -31,11 +31,11 @@ class Stylist < ActiveRecord::Base
 
   # mount_uploader :photo, PictureUploader
 
-  # validates :first_name, presence: true, length: { maximum: 50 }
-  # validates :last_name, presence: true, length: { maximum: 50 }
-  # VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  # validates :stylist_email, presence: true, format: { with: VALID_EMAIL_REGEX },
-  #                   uniqueness: { case_sensitive: false }
-  # validates :password, length: { minimum: 6 }
-  # validates :password_confirmation, presence: true
+  validates :first_name, presence: true, length: { maximum: 50 }
+  validates :last_name, presence: true, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :stylist_email, presence: true, format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+  validates :password, length: { minimum: 6 }
+  validates :password_confirmation, presence: true
 end

640ae553d699ba55525e47645f11448367b0d690
Derrick Lannaman <derricklannaman@gmail.com>
2013-01-09 13:06:03 -0500

    change email address for mailer, update email.text.erb

--- app/mailers/notifications.rb
+++ app/mailers/notifications.rb
@@ -1,11 +1,11 @@
 class Notifications < ActionMailer::Base
-  default from: "derricklannaman@gmail.com"
+  default from: "beautiflowme@gmail.com"
 
 
   def welcome_email(stylist)
     @stylist = stylist
-    @greeting = "Hi"
+    @url = "http://damp-badlands-6555.herokuapp.com/"
 
-    mail to: "derricklannaman@gmail.com", :subject => "Welcome to Beautiflow"
+    mail to: stylist_email, :subject => "Welcome to Beautiflow"
   end
 end
